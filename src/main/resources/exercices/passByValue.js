0fc5abd84a8a45138a4d85659fd6e3efbcd8d9d8
Vlad <arantir89@gmail.com>
2019-09-13 18:46:31 +0300

    comments

--- src/main/resources/exercices/passByValue.js
+++ src/main/resources/exercices/passByValue.js
@@ -1,18 +1,28 @@
 let primitive ='hi';
-let referenced = {oho: 48};
+let referenced = {
+    oho: 48,
+    // toString: () => `this[uhu] = ${this['uhu']}, this[oho] = ${this['oho']}` // так нельзя: стрелочная функция не имеет this
+    toString: () => `this[uhu] = ${this['uhu']}, this[oho] = ${this['oho']}`
+};
 
+// в методе заменяем значение переменной, переданной в аргументы, на новое и возвращаем его;
+// изменится ли значение исходной переменной?
 function changeArgValue(arg) {
     arg = {uhu: 25,
     toString: function () {
-        return `this.uhu = ${this.uhu}`;
+        return `this[uhu] = ${this['uhu']}, this[oho] = ${this['oho']}`;
     }};
     return arg;
 }
-changeArgValue.apply(referenced, )
+// changeArgValue.apply(referenced, );
+
+alert(referenced.oho);
+alert(`before handling: primitive = ${primitive}`);
 let changed = changeArgValue(primitive);
-alert('after handling: primitive = ' + primitive + ', changed = ' + changed);
+alert(`after handling: primitive = ${primitive}, changed = ${changed}`); // значение исходной переменной не поменялось
 
+alert(`before handling: referenced = ${referenced}`);
 changed = changeArgValue(referenced);
-alert('after handling: referenced = ' + referenced + ', changed = ' + changed);
+alert(`after handling: primitive = ${primitive}, changed = ${changed}`); // значение исходной переменной не поменялось
 
-
+// в этом методе
